a38957b56cc3a6b26fc23e5346bea1732085c7b1
Yin-Yanshu <2120260040@qq.com>
2025-02-16 22:35:45 +0800

    feat(FlexWrapper): 新增弹性栅格展示容器组件

---
+++ src/components/FlexWrapper/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex-box-wrap">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+  // import { computed, defineProps } from 'vue';
+  //
+  // const props = defineProps({
+  //   gap: {
+  //     type: Number || String,
+  //     default: 10,
+  //   },
+  // });
+  // const style = computed(() => {
+  //   if (typeof props.gap === 'number') {
+  //     return {
+  //       // grid布局属性，设置各元素上下左右间距
+  //       gap: `${props.gap}px`,
+  //     };
+  //   }
+  //   if (typeof props.gap === 'string') {
+  //     return {
+  //       gap: props.gap,
+  //     };
+  //   } else {
+  //     return {
+  //       gap: '15px',
+  //     };
+  //   }
+  // });
+</script>
+
+<style scoped>
+  .flex-box-wrap {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-content: space-around;
+    gap: 15px;
+  }
+</style>
